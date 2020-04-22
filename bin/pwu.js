e38727b7fc55e684f0b37887ca4cc33c117d3e3a
pingwen <pingwen@chelun.com>
2020-04-22 10:43:17 +0800

    v1.0.0

--- bin/pwu.js
+++ bin/pwu.js
@@ -2,16 +2,35 @@
 const ora = require('ora');
 const chalk = require('chalk');
 const program = require('commander');
+const download = require('download-git-repo');
 
 program
-    .version('1.0.0', '-v, --version', '版本')
-    .parse(process.argv);
+    .version('1.0.0', '-v, --version', '版本');
 
+program
+  .command('init [destination]')
+  .description('clone a repository into a newly created directory')
+  .action(destination => {
+    const spinner = ora('开始下载脚手架');
+    spinner.start();
+	destination = destination || process.cwd();
+	//console.log(destination);
+	download('github:pwstrick/pwu', destination, (msg) => {
+      spinner.stop();
+	  console.log(msg);
+	  console.log(chalk.green('脚手架下载成功'));
+	});
+  });
+
+program.parse(process.argv)
 //const param = process.argv[3] || "init";
+// function init() {
+  // const spinner = ora('开始下载脚手架');
+  // spinner.start();
+  // setTimeout(() => {
+    // spinner.stop();
+    // console.log(chalk.green('脚手架下载成功'));
+  // }, 3000);
 
-const spinner = ora('开始下载');
-spinner.start();
-setTimeout(() => {
-    spinner.stop();
-    console.log(chalk.green('模板下载成功'));
-}, 3000);
+// }
+
